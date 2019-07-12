88be80dc2cf4dc50ac93300d3ebbf84a52ad9bcc
ajayarankeri <ajayarankeri@gmail.com>
2019-07-12 18:06:32 +0530

    initial commit

--- src/main/java/com/hcl/LoanOffersOnMortgage/controller/UserDetailsController.java
+++ src/main/java/com/hcl/LoanOffersOnMortgage/controller/UserDetailsController.java
@@ -1,16 +1,16 @@
 package com.hcl.LoanOffersOnMortgage.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hcl.LoanOffersOnMortgage.entity.Offers;
-import com.hcl.LoanOffersOnMortgage.entity.PropertyRates;
 import com.hcl.LoanOffersOnMortgage.entity.User;
 import com.hcl.LoanOffersOnMortgage.service.OfferServices;
 import com.hcl.LoanOffersOnMortgage.service.PropertyRatesService;
@@ -35,11 +35,12 @@ public class UserDetailsController {
 	*/	
 	
 	@PostMapping("/userDetails")
-	public void addUserDetails(@RequestBody User user) {
+	public ResponseEntity<List<Offers>> addUserDetails(@RequestBody User user) {
 
 		List<Offers> propertyList=propertyRatesService.checkOfferapplicable(user);
 		
 		System.out.println(propertyList);
+		return new ResponseEntity<List<Offers>>(propertyList, HttpStatus.OK);
 	}
 
 }
